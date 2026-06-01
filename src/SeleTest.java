823f386bee2485d019df767cd1be970ff26e6d7f
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-06-02 00:34:16 +0530

    open google in Edge & maximize it

--- src/SeleTest.java
+++ src/SeleTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 
 public class SeleTest {
@@ -8,7 +9,8 @@ public class SeleTest {
 	public static void main(String[] args) {
 		
 //		WebDriver driver = new ChromeDriver();
-		WebDriver driver = new FirefoxDriver();
+//		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new EdgeDriver();
 		
 		driver.get("https://www.google.com"); // open google.com using get() method
 		driver.manage().window().maximize(); // maximize the browser window
