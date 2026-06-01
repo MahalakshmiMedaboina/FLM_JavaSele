34c000fce64f3d2bb94c5e431f2c8cbfc7479dbc
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-06-02 00:33:06 +0530

    open google in firefox & maximize it

--- src/SeleTest.java
+++ src/SeleTest.java
@@ -1,18 +1,18 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class SeleTest {
 
 	public static void main(String[] args) {
 		
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("https://www.google.com"); // open google.com using get() method
 		driver.manage().window().maximize(); // maximize the browser window
-		
-		
-		
+
 
 	}
 
